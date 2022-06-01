e3c6e37ae142bdd70c1dded6830b080732e7ef85
Moazzam19 <moazzam@syntaxtechs.com>
2022-05-31 21:52:20 -0400

    2nd commit

--- src/gitReview/gitreview01.java
+++ src/gitReview/gitreview01.java
@@ -2,4 +2,13 @@ package gitReview;
 
 public class gitreview01 {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("today we learn github");
+		System.out.print("today is a very good dAY");
+
+		System.out.print("today is a very good dAY");
+		 
+	}
+
 }
